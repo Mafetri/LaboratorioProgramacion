3ee2dcaaf298582e9a0a470207ed7e48876bcf68
Manuel Triñanes <manutritri2@gmail.com>
2022-08-22 19:22:35 -0300

    Ejemplo Funcional

---
+++ PowRecursiveAction.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class PowRecursiveAction {
+	public static void main(String[] args) {
+		double [] data = new double[10000000];
+        for(int i = 1; i < data.length; i++){
+            data[i] = 134/i;
+        }
+
+        long startTime = System.nanoTime();
+		ForkJoinPool pool = new ForkJoinPool();
+		Pow app = new Pow(data, 0, data.length);
+		pool.invoke(app);
+        long endTime = System.nanoTime();
+
+        System.out.println("Tiempo: " + (endTime - startTime)/100000);
+	}
+}
+class Pow extends RecursiveAction {
+    final int threshold = 10000;
+    int start, end;
+    double[] data;
+    Pow(double[] data, int start, int end) {
+        this.start = start;
+        this.end = end;
+        this.data = data;
+    }
+
+    @Override
+    protected void compute() {
+        if((end - start) < threshold){
+            for(int i = start; i < end; i++){
+                data[i] = Math.pow(data[i], 5);
+            }
+        }else {
+            int mid = (start + end)/2;
+            invokeAll(new Pow(data, start, mid),new Pow(data, mid, end));
+        }
+    }
+}
---
+++ PowSingle.java
@@ -0,0 +1,16 @@
+public class PowSingle {
+	public static void main(String[] args) {
+		double[] data = new double[10000000];
+        for(int i = 1; i < data.length; i++){
+            data[i] = 134/i;
+        }
+
+        long startTime = System.nanoTime();
+        for(int i = 0; i < data.length; i++){
+            data[i] = Math.pow(data[i], 5);
+        }
+
+        long endTime = System.nanoTime();
+        System.out.println("Tiempo: " + (endTime - startTime)/100000);
+	}
+}
