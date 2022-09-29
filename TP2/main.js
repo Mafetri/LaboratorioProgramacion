dcc2313f485a514281df8af44b978791cdb61cba
Manuel Triñanes <manutritri2@gmail.com>
2022-09-29 19:47:40 -0300

    apurado

--- TP2/main.js
+++ TP2/main.js
@@ -19,7 +19,13 @@ window.addEventListener("scroll", function(){
 
 const toggleButton = document.getElementsByClassName('nav-button')[0]
 const navbarLinks = document.getElementsByClassName('nav-links')[0]
+const header = document.querySelector("header");
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
+  header.classList.toggle("solido");
 })
+
+var cursos = JSON.parse(requisitosCursos);
+const edad = document.querySelector("edad");
+edad.textContent= alert(cursos[0].edad);
