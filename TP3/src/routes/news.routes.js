e400a063cb9a674d58dd048fd4c0cd0488511c50
JonathanJan7 <jonathan.jan@est.fi.uncoma.edu.ar>
2022-11-08 22:18:11 -0300

    firstServer

---
+++ TP3/src/routes/news.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { getNews, createNew, updateNew, deleteNew } from "../controllers/news.controller.js";
+
+const router=Router();
+
+router.get('/api/news', getNews);
+
+router.post('/api/news', createNew);
+
+router.put('/api/news', updateNew);
+
+router.delete('/api/news/:id', deleteNew);
+
+export default router;
