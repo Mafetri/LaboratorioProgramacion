968d2c567ca0612bd198b40633f28f98aabb1420
Manuel Triñanes <manutritri2@gmail.com>
2022-11-12 10:54:01 -0300

    TryCatch News and Fleet

--- TP2/controllers/fleet.controller.js
+++ TP2/controllers/fleet.controller.js
@@ -1,70 +1,116 @@
 // Data Base
-import {pool} from '../db.js'
+import { pool } from "../db.js";
 
 // Get Fleet, returns the fleet from x position and an 'n' ammount of airplanes
 export const getFleet = async (req, res) => {
-    let {x0, n} = req.query;
-    const [rows] = await pool.query('SELECT * FROM fleet LIMIT ?,?', [parseInt(x0),parseInt(n)]);
-    res.json(rows);
-}
+	const { x0, n } = req.query;
+
+	try {
+		const [rows] = await pool.query("SELECT * FROM fleet LIMIT ?,?", [
+			parseInt(x0),
+			parseInt(n),
+		]);
+		res.json(rows);
+	} catch (e) {
+		return res.status(500).json({
+			message: "Something went wrong",
+			error: e,
+		});
+	}
+};
 
 // Get Airplane, returns just one airplane by plate
 export const getAirplane = async (req, res) => {
-    const [ airplane ] = await pool.query('SELECT * FROM fleet WHERE plate = ?', [req.params.plate]);
+	try {
+		const [airplane] = await pool.query("SELECT * FROM fleet WHERE plate = ?", [
+			req.params.plate,
+		]);
 
-    if(airplane.length === 0){
-        res.status(400).json({
-            message: "Airplane not found"
-        })
-    }else{
-        res.json(airplane[0]);
-    }
-}
+		if (airplane.length === 0) {
+			res.status(400).json({
+				message: "Airplane not found",
+			});
+		} else {
+			res.json(airplane[0]);
+		}
+	} catch (e) {
+		return res.status(500).json({
+			message: "Something went wrong",
+			error: e,
+		});
+	}
+};
 
 // Create Airplane, creates an airplane on the db
 export const createAirplane = async (req, res) => {
-    try{
-        let { plate, name, engine, brand, model, speed, consumption, img} = req.body;
-        await pool.query('INSERT INTO fleet (plate, name, engine, brand, model, speed, consumption, img) VALUES (?,?,?,?,?,?,?,?)', [plate, name, engine, brand, model, speed, consumption, img])
-        res.send('Post Success')
-    } catch(e){
-        if(e.code = "ER_DUP_ENTRY"){
-            return res.status(500).json({
-                message: "Error, Duplicated Plate"
-            })
-        }else{
-            return res.status(500).json({
-                message: "Error"
-            })
-        }
-        
-    }
-    
-}
+	const { plate, name, engine, brand, model, speed, consumption, img } = req.body;
+
+	try {
+		await pool.query(
+			"INSERT INTO fleet (plate, name, engine, brand, model, speed, consumption, img) VALUES (?,?,?,?,?,?,?,?)",
+			[plate, name, engine, brand, model, speed, consumption, img],
+		);
+		res.send("Post Success");
+	} catch (e) {
+		if ((e.code = "ER_DUP_ENTRY")) {
+			return res.status(500).json({
+				message: "Error, duplicated plate",
+			});
+		} else {
+			return res.status(500).json({
+				message: "Something went wrong",
+				error: e,
+			});
+		}
+	}
+};
 
 // Update Airplane, modifies the info of an existing airplane
 export const updateAirplane = async (req, res) => {
-    const { plate } = req.params;
-    const { name, engine, brand, model, speed, consumption, img } = req.body;
-    const [ dbRes ] = await pool.query('UPDATE fleet SET name = IFNULL(?, name), engine = IFNULL(?, engine), brand = IFNULL(?, brand), model = IFNULL(?, model), speed = IFNULL(?, speed), consumption = IFNULL(?, consumption), img = IFNULL(?, img) WHERE plate = ?', [ name, engine, brand, model, speed, consumption, img, plate ]);
-    
-    if(dbRes.affectedRows === 0){
-        return res.status(404).json({
-            message: "Airplane not found"
-        })
-    } else{
-        res.json((await pool.query('SELECT * FROM fleet WHERE plate = ?', [plate]))[0]);
-    }
-}
+	const { plate } = req.params;
+	const { name, engine, brand, model, speed, consumption, img } = req.body;
+
+	try {
+		const [dbRes] = await pool.query(
+			"UPDATE fleet SET name = IFNULL(?, name), engine = IFNULL(?, engine), brand = IFNULL(?, brand), model = IFNULL(?, model), speed = IFNULL(?, speed), consumption = IFNULL(?, consumption), img = IFNULL(?, img) WHERE plate = ?",
+			[name, engine, brand, model, speed, consumption, img, plate],
+		);
+
+		if (dbRes.affectedRows === 0) {
+			return res.status(404).json({
+				message: "Airplane not found",
+			});
+		} else {
+			res.json(
+				(await pool.query("SELECT * FROM fleet WHERE plate = ?", [plate]))[0],
+			);
+		}
+	} catch (e) {
+		return res.status(500).json({
+			message: "Error",
+			error: e,
+		});
+	}
+};
 
 // Delete Airplane, deletes a given plate airplane from de db
 export const deleteAirplane = async (req, res) => {
-    const { id } = req.params;
-    const [ dbRes ] = await pool.query('DELETE FROM news WHERE id=?',[id]);
+	const { id } = req.params;
 
-    if(dbRes.affectedRows === 0){ return res.status(404).json({
-        message: "Airplane not found"
-    })}else{
-        res.send("News Deleted");
-    }
-}
+	try {
+		const [dbRes] = await pool.query("DELETE FROM news WHERE id=?", [id]);
+
+		if (dbRes.affectedRows === 0) {
+			return res.status(404).json({
+				message: "Airplane not found",
+			});
+		} else {
+			res.send("News Deleted");
+		}
+	} catch (e) {
+		return res.status(500).json({
+			message: "Error",
+			error: e,
+		});
+	}
+};
--- TP2/controllers/news.controller.js
+++ TP2/controllers/news.controller.js
@@ -1,43 +1,85 @@
 // Data Base
-import {pool} from '../db.js'
+import { pool } from "../db.js";
 
 // Get news
 export const getNews = async (req, res) => {
-    let {x0, n} = req.query;
-    const [rows] = await pool.query('SELECT * FROM news ORDER BY date DESC LIMIT ?,?', [parseInt(x0),parseInt(n)]);
-    res.json(rows);
-}
+	const { x0, n } = req.query;
+
+	try {
+		const [rows] = await pool.query(
+			"SELECT * FROM news ORDER BY date DESC LIMIT ?,?",
+			[parseInt(x0), parseInt(n)],
+		);
+		res.json(rows);
+	} catch (e) {
+		return res.status(500).json({
+			message: "Something went wrong",
+			error: e,
+		});
+	}
+};
 
 // Create news
 export const createNews = async (req, res) => {
-    let { date, title, description, img } = req.body;
-    await pool.query('INSERT INTO news (date, title, description, img) VALUES (?,?,?,?)', [date, title, description, img])
-    res.send('Post Success')
+	const { date, title, description, img } = req.body;
+
+	try {
+		await pool.query(
+			"INSERT INTO news (date, title, description, img) VALUES (?,?,?,?)",
+			[date, title, description, img],
+		);
+		res.send("Post Success");
+	} catch (e) {
+		return res.status(500).json({
+			message: "Something went wrong",
+			error: e,
+		});
+	}
 };
 
 // Update news
 export const updateNews = async (req, res) => {
-    const { id } = req.params;
-    const { date, title, description, img } = req.body;
-    const [ dbRes ] = await pool.query('UPDATE news SET date = IFNULL(?, date), title = IFNULL(?, title), description = IFNULL(?, description), img = IFNULL(?, img) WHERE id = ?', [date, title, description, img, id]);
-    
-    if(dbRes.affectedRows === 0) return res.status(404).json({
-        message: "New not found"
-    })
-    
-    res.json((await pool.query('SELECT * FROM news WHERE id = ?', [id]))[0]);
+	const { id } = req.params;
+	const { date, title, description, img } = req.body;
+
+	try {
+		const [dbRes] = await pool.query(
+			"UPDATE news SET date = IFNULL(?, date), title = IFNULL(?, title), description = IFNULL(?, description), img = IFNULL(?, img) WHERE id = ?",
+			[date, title, description, img, id],
+		);
+
+		if (dbRes.affectedRows === 0)
+			return res.status(404).json({
+				message: "New not found",
+			});
+
+		res.json((await pool.query("SELECT * FROM news WHERE id = ?", [id]))[0]);
+	} catch (e) {
+		return res.status(500).json({
+			message: "Something went wrong",
+			error: e,
+		});
+	}
 };
 
 // Delete news
 export const deleteNews = async (req, res) => {
-    const { id } = req.params;
-    const [ dbRes ] = await pool.query('DELETE FROM news WHERE id=?',[id]);
+	const { id } = req.params;
 
-    if(dbRes.affectedRows === 0){
-        return res.status(404).json({
-            message: "New not found"
-        })
-    }else{
-        res.send("News Deleted");
-    }
+	try {
+		const [dbRes] = await pool.query("DELETE FROM news WHERE id=?", [id]);
+
+		if (dbRes.affectedRows === 0) {
+			return res.status(404).json({
+				message: "New not found",
+			});
+		} else {
+			res.send("News Deleted");
+		}
+	} catch (e) {
+		return res.status(500).json({
+			message: "Something went wrong",
+			error: e,
+		});
+	}
 };
