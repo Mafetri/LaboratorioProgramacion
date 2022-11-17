272973c79dfdeef3d2b93efd96b3ecb2ffed37fd
Manuel Triñanes <manutritri2@gmail.com>
2022-11-17 14:58:08 -0300

    SRC Folder

--- TP2/src/app.js
+++ TP2/src/app.js
@@ -5,13 +5,11 @@ import weatherRoutes from "./routes/weather.routes.js";
 import coursesRoutes from "./routes/courses.routes.js";
 import trajectoryRoutes from "./routes/trajectory.routes.js";
 import submitForm from "./routes/form.routes.js";
-import flash from "connect-flash"
 
 const app= express();
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(flash());
 
 // APIs
 app.use("/api", newsRoutes);
@@ -22,6 +20,6 @@ app.use("/api", trajectoryRoutes);
 app.use("/api", submitForm);
 
 // Static webpage
-app.use(express.static('static'));
+app.use(express.static('src/static'));
 
 export default app;
