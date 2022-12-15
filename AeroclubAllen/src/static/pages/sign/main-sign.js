f870aa752f4e9b8e5629111f89c3c96ce6c4cbf1
Manuel Triñanes <manutritri2@gmail.com>
2022-12-15 15:45:08 -0300

    Styles on Sign phone

--- AeroclubAllen/src/static/pages/sign/main-sign.js
+++ AeroclubAllen/src/static/pages/sign/main-sign.js
@@ -1,12 +1,26 @@
+let x = window.matchMedia("(max-width: 768px)");
+
 let imgLeft = true;
-document.querySelector("#sign-cover").onclick = ()=>{
-	if(imgLeft){
-		document.querySelector("#sign-cover").style = "translate: 35em 0em";
-		document.querySelector("#sign-cover h2").textContent = "Ingresar"
-		imgLeft = false;
+document.querySelector("#sign-cover").onclick = () => {
+	if (x.matches){
+		if(imgLeft){
+			document.querySelector("#sign-cover").style = "translate: 0em -35em";
+			document.querySelector("#sign-cover h2").textContent = "Ingresar"
+			imgLeft = false;
+		} else {
+			document.querySelector("#sign-cover").style = "translate: 0em 0em";
+			document.querySelector("#sign-cover h2").textContent = "Registrarse"
+			imgLeft = true;
+		}
 	} else {
-		document.querySelector("#sign-cover").style = "translate: 0em 0em";
-		document.querySelector("#sign-cover h2").textContent = "Registrarse"
-		imgLeft = true;
+		if(imgLeft){
+			document.querySelector("#sign-cover").style = "translate: 25em 0em";
+			document.querySelector("#sign-cover h2").textContent = "Ingresar"
+			imgLeft = false;
+		} else {
+			document.querySelector("#sign-cover").style = "translate: 0em 0em";
+			document.querySelector("#sign-cover h2").textContent = "Registrarse"
+			imgLeft = true;
+		}
 	}
-}
+};
